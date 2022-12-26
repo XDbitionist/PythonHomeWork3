e8e6575dca88e4396f6ad52040a8cc0a3c988e1e
XDbitionist <eugene.sakharov@gmail.com>
2022-12-26 21:13:28 +0800

    finished task 2

--- Lesson 3. Python/Task2.py
+++ Lesson 3. Python/Task2.py
@@ -2,3 +2,17 @@
 # Пример:
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
+from random import randint as r
+def mult_lst(lst):
+    l = len(lst)//2 + 1 if len(lst) % 2 != 0 else len(lst)//2
+    new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
+    print(new_lst)
+
+lst = [2, 3, 4, 5, 6]
+lst2 = [2, 3, 5, 6]
+print(lst)
+mult_lst(lst)
+print()
+print(lst2)
+mult_lst(lst2)
+
